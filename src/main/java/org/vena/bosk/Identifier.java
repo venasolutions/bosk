6bd623b7806576e1a1d121ff45e3429e6348d288
Patrick Doyle <pdoyle@venacorp.com>
2022-06-17 13:02:13 -0400

    Disallow empty Identifiers

--- src/main/java/org/vena/bosk/Identifier.java
+++ src/main/java/org/vena/bosk/Identifier.java
@@ -12,6 +12,10 @@ public final class Identifier {
 
 	// TODO: Intern these.  No need to have several Identifier objects for the same value
 	public static Identifier from(String value) {
+		if (value.isEmpty()) {
+			// TODO: We probably want more rules than this. Like: no starting/ending with a dash.
+			throw new IllegalArgumentException("Identifier can't be empty");
+		}
 		return new Identifier(value);
 	}
 
