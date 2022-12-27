fab8e027beca5ce7da10024b4322b3a1090bed0a
Patrick Doyle <pdoyle@venacorp.com>
2022-12-26 22:01:24 -0500

    Switch CatalogBenchmark to JMH

--- bosk-core/src/test/java/io/vena/bosk/CatalogBenchmark.java
+++ bosk-core/src/test/java/io/vena/bosk/CatalogBenchmark.java
@@ -3,43 +3,68 @@ package io.vena.bosk;
 import io.vena.bosk.AbstractBoskTest.TestEntity;
 import io.vena.bosk.AbstractBoskTest.TestRoot;
 import io.vena.bosk.exceptions.InvalidTypeException;
-import io.vena.bosk.junit.PerformanceTest;
+import java.util.LinkedHashMap;
 import java.util.stream.IntStream;
-import org.junit.jupiter.api.BeforeEach;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
 
 import static io.vena.bosk.AbstractBoskTest.TestEnum.OK;
-import static io.vena.bosk.MicroBenchmark.callingMethodInfo;
+import static org.openjdk.jmh.annotations.Mode.Throughput;
 
+@Fork(0)
+@Warmup(iterations = 5, time = 1)
+@Measurement(iterations = 10, time = 1)
 public class CatalogBenchmark {
-	private TestEntityBuilder teb;
 
-	@BeforeEach
-	void setup() throws InvalidTypeException {
-		Bosk<TestRoot> bosk = new Bosk<TestRoot>(
-			"CatalogBenchmarkBosk",
-			TestRoot.class,
-			AbstractBoskTest::initialRoot,
-			Bosk::simpleDriver
-		);
-		teb = new TestEntityBuilder(bosk);
+	@State(Scope.Benchmark)
+	public static class BenchmarkState {
+		private Catalog<TestEntity> catalog;
+		private LinkedHashMap<Identifier, TestEntity> map;
+		private TestEntity newEntity;
+
+		@Setup(Level.Trial)
+		public void setup() throws InvalidTypeException {
+			Bosk<TestRoot> bosk = new Bosk<TestRoot>(
+				"CatalogBenchmarkBosk",
+				TestRoot.class,
+				AbstractBoskTest::initialRoot,
+				Bosk::simpleDriver
+			);
+			TestEntityBuilder teb = new TestEntityBuilder(bosk);
+			int initialSize = 100_000;
+			catalog = Catalog.of(IntStream.rangeClosed(1, initialSize).mapToObj(i ->
+				teb.blankEntity(Identifier.from("Entity_" + i), OK)));
+			map = new LinkedHashMap<>();
+			catalog.forEach(e -> map.put(e.id(), e));
+			newEntity = teb.blankEntity(Identifier.from("New entity"), OK);
+		}
 	}
 
-	@PerformanceTest
-	void performanceTest_with() {
-		int initialSize = 100_000;
-		Catalog<TestEntity> catalog = Catalog.of(IntStream.rangeClosed(1, initialSize).mapToObj(i ->
-			teb.blankEntity(Identifier.from("Entity_" + i), OK)));
-		TestEntity newEntity = teb.blankEntity(Identifier.from("New entity"), OK);
-		new MicroBenchmark(callingMethodInfo()) {
-			@Override
-			protected void doIterations(long count) {
-				for (long i = 0; i < count; i++) {
-					escape = catalog.with(newEntity);
-				}
-			}
-		}.computeRate();
+	@Benchmark
+	@BenchmarkMode(Throughput)
+	public Object catalogWith_sameEntity(BenchmarkState state) {
+		return state.catalog.with(state.newEntity);
 	}
 
-	public static Object escape;
+	@Benchmark
+	@BenchmarkMode(Throughput)
+	public Object newLinkedHashMap_sameEntity(BenchmarkState state) {
+		LinkedHashMap<Identifier, TestEntity> result = new LinkedHashMap<>(state.map);
+		result.put(state.newEntity.id(), state.newEntity);
+		return result;
+	}
+
+	@Benchmark
+	@BenchmarkMode(Throughput)
+	public Object linkedHashMapPut_sameEntity(BenchmarkState state) {
+		return state.map.put(state.newEntity.id(), state.newEntity);
+	}
 
 }
