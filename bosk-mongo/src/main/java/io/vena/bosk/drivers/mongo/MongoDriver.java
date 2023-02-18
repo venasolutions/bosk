7295254f16a3a34677d017b2e6a5dd66bcdea9a8
Patrick Doyle <pdoyle@venacorp.com>
2023-02-18 16:08:29 -0500

    Refactor: move javadocs to MongoDriver

--- bosk-mongo/src/main/java/io/vena/bosk/drivers/mongo/MongoDriver.java
+++ bosk-mongo/src/main/java/io/vena/bosk/drivers/mongo/MongoDriver.java
@@ -7,7 +7,31 @@ import io.vena.bosk.DriverFactory;
 import io.vena.bosk.Entity;
 
 public interface MongoDriver<R extends Entity> extends BoskDriver<R> {
+	/**
+	 * Deserializes and re-serializes the entire bosk contents,
+	 * thus updating the database to match the current serialized format.
+	 *
+	 * <p>
+	 * Used to "upgrade" the database contents for schema evolution.
+	 *
+	 * <p>
+	 * This method does not simply write the current in-memory bosk contents
+	 * back into the database, because that would lead to race conditions
+	 * with other update operations.
+	 * Instead, in a causally-consistent transaction, it reads the current
+	 * database state, deserializes it, re-serializes it, and writes it back.
+	 * This produces predictable results even if done concurrently with
+	 * other database updates.
+	 */
 	void refurbish();
+
+	/**
+	 * Frees up resources used by this driver and leaves it unusable.
+	 *
+	 * <p>
+	 * This is done on a best-effort basis. It's more useful for tests than for production code,
+	 * where there's usually no reason to close a driver.
+	 */
 	void close();
 
 	static <RR extends Entity> MongoDriverFactory<RR> factory(
