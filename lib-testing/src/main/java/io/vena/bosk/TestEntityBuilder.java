af7ebfb237b34e27649a730d55e635d41fd33d98
Patrick Doyle <pdoyle@venacorp.com>
2023-03-06 14:46:59 -0500

    Use buildReferences for TestEntityBuilder

--- lib-testing/src/main/java/io/vena/bosk/TestEntityBuilder.java
+++ lib-testing/src/main/java/io/vena/bosk/TestEntityBuilder.java
@@ -7,26 +7,29 @@ import io.vena.bosk.AbstractBoskTest.TestChild;
 import io.vena.bosk.AbstractBoskTest.TestEntity;
 import io.vena.bosk.AbstractBoskTest.TestEnum;
 import io.vena.bosk.AbstractBoskTest.TestRoot;
+import io.vena.bosk.annotations.ReferencePath;
 import io.vena.bosk.exceptions.InvalidTypeException;
 
 public class TestEntityBuilder {
-	private final CatalogReference<TestEntity> entitiesRef;
-	private final Reference<TestEntity> anyEntity;
-	private final CatalogReference<TestChild> anyChildren;
-	private final Reference<ImplicitRefs> anyImplicitRefs;
+	public final Refs refs;
 
-	public TestEntityBuilder(Bosk<TestRoot> bosk) throws InvalidTypeException {
-		this.entitiesRef = bosk.rootReference().thenCatalog(TestEntity.class, TestRoot.Fields.entities);
-		this.anyEntity = bosk.rootReference().then(TestEntity.class, TestRoot.Fields.entities, "-entity-");
-		this.anyChildren = anyEntity.thenCatalog(TestChild.class, TestEntity.Fields.children);
-		this.anyImplicitRefs = anyEntity.then(ImplicitRefs.class, TestEntity.Fields.implicitRefs);
+	public interface Refs {
+		@ReferencePath("/entities") CatalogReference<TestEntity> entitiesRef();
+		@ReferencePath("/entities/-entity-") Reference<TestEntity> anyEntity();
+		@ReferencePath("/entities/-entity-") Reference<TestEntity> entityRef(Identifier entity);
+		@ReferencePath("/entities/-entity-/children") CatalogReference<TestChild> childrenRef(Identifier entity);
+		@ReferencePath("/entities/-entity-/implicitRefs") Reference<ImplicitRefs> implicitRefsRef(Identifier entity);
 	}
 
-	public CatalogReference<TestEntity> entitiesRef() { return entitiesRef; }
-	public Reference<TestEntity> anyEntity() { return anyEntity; }
-	public Reference<TestEntity> entityRef(Identifier id) { return anyEntity.boundTo(id); }
-	public CatalogReference<TestChild> childrenRef(Identifier entityID) { return anyChildren.boundTo(entityID); }
-	public Reference<ImplicitRefs> implicitRefsRef(Identifier entityID) { return anyImplicitRefs.boundTo(entityID); }
+	public TestEntityBuilder(Bosk<TestRoot> bosk) throws InvalidTypeException {
+		this.refs = bosk.buildReferences(Refs.class);
+	}
+
+	public CatalogReference<TestEntity> entitiesRef() { return refs.entitiesRef(); }
+	public Reference<TestEntity> anyEntity() { return refs.anyEntity(); }
+	public Reference<TestEntity> entityRef(Identifier id) { return refs.entityRef(id); }
+	public CatalogReference<TestChild> childrenRef(Identifier entityID) { return refs.childrenRef(entityID); }
+	public Reference<ImplicitRefs> implicitRefsRef(Identifier entityID) { return refs.implicitRefsRef(entityID); }
 
 	public TestEntity blankEntity(Identifier id, TestEnum testEnum) {
 		return new TestEntity(id,
