cd59886baa9c379ce34bb97928e376e87c587691
Patrick Doyle <pdoyle@venacorp.com>
2023-03-09 18:25:54 -0500

    Create bosk-testing library

--- bosk-testing/src/main/java/io/vena/bosk/junit/ParametersByNameContextProvider.java
+++ bosk-testing/src/main/java/io/vena/bosk/junit/ParametersByNameContextProvider.java
@@ -22,7 +22,7 @@ import static java.util.Collections.emptyMap;
 import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.toList;
 
-public class ParametersByNameContextProvider implements TestTemplateInvocationContextProvider {
+class ParametersByNameContextProvider implements TestTemplateInvocationContextProvider {
 
 	@Override
 	public boolean supportsTestTemplate(ExtensionContext context) {
