3e47cc90995fcc39afcdc426f5eb750dacff1938
Patrick Doyle <pdoyle@venacorp.com>
2023-06-02 05:19:07 -0400

    Fix race in FlushLock

--- bosk-mongo/src/test/java/io/vena/bosk/drivers/mongo/TestParameters.java
+++ bosk-mongo/src/test/java/io/vena/bosk/drivers/mongo/TestParameters.java
@@ -4,7 +4,8 @@ import io.vena.bosk.drivers.mongo.MongoDriverSettings.MongoDriverSettingsBuilder
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
-import static io.vena.bosk.drivers.mongo.MongoDriverSettings.FlushMode.ECHO;
+import static io.vena.bosk.drivers.mongo.MongoDriverSettings.ImplementationKind.RESILIENT;
+import static io.vena.bosk.drivers.mongo.MongoDriverSettings.ImplementationKind.STABLE;
 
 public interface TestParameters {
 	AtomicInteger dbCounter = new AtomicInteger(0);
@@ -15,20 +16,10 @@ public interface TestParameters {
 		return Stream.of(
 			MongoDriverSettings.builder()
 				.database(prefix + "_stable")
-				.flushMode(ECHO)
-//			MongoDriverSettings.builder()
-//				.database(prefix + "_resilient")
-//				.implementationKind(RESILIENT)
-			// These tests fail too often. REVISION_FIELD_ONLY is not reliable yet.
-//			MongoDriverSettings.builder()
-//				.database(prefix + "_rev")
-//				.flushMode(REVISION_FIELD_ONLY),
-//			MongoDriverSettings.builder()
-//				.database(prefix + "_slow")
-//				.flushMode(REVISION_FIELD_ONLY)
-//				.testing(MongoDriverSettings.Testing.builder()
-//					.eventDelayMS(100)
-//					.build())
+				.implementationKind(STABLE),
+			MongoDriverSettings.builder()
+				.database(prefix + "_resilient")
+				.implementationKind(RESILIENT)
 		);
 	}
 
