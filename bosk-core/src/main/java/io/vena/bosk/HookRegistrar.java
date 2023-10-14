f2ff95731b4d357fbd356fdda8cbdc2179defb2e
Patrick Doyle <pdoyle@venacorp.com>
2023-10-14 13:23:55 -0400

    Add @Hook annotation.
    Even though it's the same as @ReferencePath, it seems confusing to use
    the same annotation for two such different purposes.

--- bosk-core/src/main/java/io/vena/bosk/HookRegistrar.java
+++ bosk-core/src/main/java/io/vena/bosk/HookRegistrar.java
@@ -1,6 +1,6 @@
 package io.vena.bosk;
 
-import io.vena.bosk.annotations.ReferencePath;
+import io.vena.bosk.annotations.Hook;
 import io.vena.bosk.exceptions.InvalidTypeException;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
@@ -10,18 +10,29 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static java.lang.reflect.Modifier.isPrivate;
+import static java.lang.reflect.Modifier.isStatic;
 
 @RequiredArgsConstructor
 class HookRegistrar {
-	@SuppressWarnings({"unchecked","rawtypes"})
 	static <T> void registerHooks(T receiverObject, Bosk<?> bosk) throws InvalidTypeException {
 		Class<?> receiverClass = receiverObject.getClass();
+		int hookCounter = 0;
 		for (Method method: receiverClass.getDeclaredMethods()) { // TODO: Inherited methods
-			ReferencePath referencePath = method.getAnnotation(ReferencePath.class);
-			if (referencePath == null) {
+			Hook hookAnnotation = method.getAnnotation(Hook.class);
+			if (hookAnnotation == null) {
 				continue;
 			}
-			Path path = Path.parseParameterized(referencePath.value());
+			if (isStatic(method.getModifiers())) {
+				throw new IllegalArgumentException("Hook method cannot be static: " + method);
+			} else if (isPrivate(method.getModifiers())) {
+				throw new IllegalArgumentException("Hook method cannot be private: " + method);
+			}
+			method.setAccessible(true);
+			Path path = Path.parseParameterized(hookAnnotation.value());
 			Reference<Object> scope = bosk.rootReference().then(Object.class, path);
 			List<Function<Reference<?>, Object>> argumentFunctions = new ArrayList<>(method.getParameterCount());
 			argumentFunctions.add(ref -> receiverObject); // The "this" pointer
@@ -30,12 +41,12 @@ class HookRegistrar {
 					if (ReferenceUtils.parameterType(p.getParameterizedType(), Reference.class, 0).equals(scope.targetType())) {
 						argumentFunctions.add(ref -> ref);
 					} else {
-						throw new InvalidTypeException("Expected reference to " + scope.targetType() + ": " + method.getName() + " parameter " + p.getName());
+						throw new IllegalArgumentException("Expected reference to " + scope.targetType() + ": " + method.getName() + " parameter " + p.getName());
 					}
 				} else if (p.getType().isAssignableFrom(BindingEnvironment.class)) {
 					argumentFunctions.add(ref -> scope.parametersFrom(ref.path()));
 				} else {
-					throw new InvalidTypeException("Unsupported parameter type " + p.getType() + ": " + method.getName() + " parameter " + p.getName());
+					throw new IllegalArgumentException("Unsupported parameter type " + p.getType() + ": " + method.getName() + " parameter " + p.getName());
 				}
 			}
 			 MethodHandle hook;
@@ -53,7 +64,14 @@ class HookRegistrar {
 					  throw new IllegalStateException("Unable to call hook \"" + method.getName() + "\"", e);
 				  }
 			 });
+			 hookCounter++;
 		}
-
+		if (hookCounter == 0) {
+			LOGGER.warn("Found no hook methods in {}; may be misconfigured", receiverObject.getClass().getSimpleName());
+		} else {
+			LOGGER.info("Registered {} hook{} in {}", hookCounter, (hookCounter >= 2)? "s":"", receiverObject.getClass().getSimpleName());
+		}
 	}
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(HookRegistrar.class);
 }
