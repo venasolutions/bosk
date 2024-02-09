dc4ba1a998409405526daf7559598520be7fe2dc
Patrick Doyle <p.r.doyle@gmail.com>
2024-02-09 16:31:53 -0500

    Add pairwise_writeCompatible and boskCounter

--- bosk-mongo/src/test/java/io/vena/bosk/drivers/mongo/SchemaEvolutionTest.java
+++ bosk-mongo/src/test/java/io/vena/bosk/drivers/mongo/SchemaEvolutionTest.java
@@ -8,6 +8,7 @@ import io.vena.bosk.drivers.mongo.MongoDriverSettings.Experimental;
 import io.vena.bosk.drivers.mongo.MongoDriverSettings.ManifestMode;
 import io.vena.bosk.drivers.state.TestEntity;
 import io.vena.bosk.junit.ParametersByName;
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.AfterEach;
@@ -87,9 +88,79 @@ public class SchemaEvolutionTest {
 		}
 	}
 
-	private static Bosk<TestEntity> newBosk(Helper helper) {
-		return new Bosk<TestEntity>(helper.name, TestEntity.class, helper::initialRoot, helper.driverFactory);
+	@ParametersByName
+	void pairwise_writeCompatible() throws Exception {
+		LOGGER.debug("Create fromBosk [{}]", fromHelper.name);
+		Bosk<TestEntity> fromBosk = newBosk(fromHelper);
+		Refs fromRefs = fromBosk.buildReferences(Refs.class);
+
+		LOGGER.debug("Create toBosk [{}]", toHelper.name);
+		Bosk<TestEntity> toBosk = newBosk(toHelper);
+		Refs toRefs = toBosk.buildReferences(Refs.class);
+
+		LOGGER.debug("Refurbish toBosk ({})", toBosk.name());
+		((MongoDriver<?>)toBosk.driver()).refurbish();
+
+		flushIfLiveRefurbishIsNotSupported(fromBosk, fromHelper, toHelper);
+
+		LOGGER.debug("Set distinctive string using fromBosk ({})", fromBosk.name());
+		fromBosk.driver().submitReplacement(fromRefs.string(), "Distinctive String");
+		LOGGER.debug("Flush fromBosk ({})", fromBosk.name());
+		fromBosk.driver().flush();
+
+		LOGGER.debug("Perform fromBosk ({}) read", fromBosk.name());
+		try (var __ = fromBosk.readContext()) {
+			assertEquals("Distinctive String", fromRefs.string().value());
+		}
+
+		LOGGER.debug("Flush toBosk({}) to see the update", toBosk.name());
+		toBosk.driver().flush();
+		
+		LOGGER.debug("Perform toBosk ({}) read", toBosk.name());
+		try (var __ = toBosk.readContext()) {
+			assertEquals("Distinctive String", toRefs.string().value());
+		}
 	}
+
+	/**
+	 * @param boskForUpdate      {@link Bosk} that is about to submit an update
+	 * @param helperForUpdate    {@link Helper} for the bosk that is about to submit an update
+	 * @param helperForRefurbish {@link Helper} for the bosk that performed the {@link MongoDriver#flush()} operation
+	 */
+	private static void flushIfLiveRefurbishIsNotSupported(
+		Bosk<TestEntity> boskForUpdate,
+		Helper helperForUpdate,
+		Helper helperForRefurbish
+	) throws IOException, InterruptedException {
+		if (helperForUpdate.driverSettings.preferredDatabaseFormat() == SEQUOIA
+			&& helperForRefurbish.driverSettings.preferredDatabaseFormat() != SEQUOIA) {
+			// When switching format classes, the old format's state documents
+			// disappear.
+			//
+			// Sequoia, being a fundamentally single-document format that does not
+			// use transactions, is not able to handle the time window between
+			// when that document disappears and when the corresponding change
+			// event arrives. We could add complexity to Sequoia to cope with
+			// this situation, but the point of Sequoia is its simplicity.
+			//
+			// Instead, we have a documented limitation that refurbishing from
+			// Sequoia to another format has a risk that updates occurring during
+			// a certain window could be ignored, and recommend that refurbish
+			// operation occur during a period of quiescence.
+			//
+			// Performing a flush causes Sequoia to "notice" its document is gone
+			// and correctly reinitialize itself.
+
+			LOGGER.debug("Flush so boskForUpdate notices the refurbish ({})", boskForUpdate.name());
+			boskForUpdate.driver().flush();
+		}
+	}
+
+	private static Bosk<TestEntity> newBosk(Helper helper) {
+		return new Bosk<TestEntity>("bosk" + boskCounter.incrementAndGet(), TestEntity.class, helper::initialRoot, helper.driverFactory);
+	}
+
+	private static final AtomicInteger boskCounter = new AtomicInteger(0);
 
 	record Configuration(
 		DatabaseFormat preferredFormat,
